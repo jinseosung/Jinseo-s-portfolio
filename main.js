0ed593074ebba3e834013dfefe63cd5bd763843a
Jinseo Sung <jinseosung15@gmail.com>
2022-09-01 10:56:10 +0200

    CSS : Responsive

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Show navbar menu when it is clicked
+const navbarMenu = document.querySelector(".navbar__menu");
+const toggleBtn = document.querySelector(".navbar__toggle-btn");
+
+toggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('navbar--selected')
+})
