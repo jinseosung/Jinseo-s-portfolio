67792f8e51ff46875cd7d01d26c0e862dca746f2
Jinseo Sung <jinseosung15@gmail.com>
2022-09-01 13:22:00 +0200

    JS : Arrow up

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ toggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("navbar--selected");
 });
 
-//Handle scrolling when tapping on the navbar menu and "contact me" button
+// Handle scrolling when tapping on the navbar menu and "contact me" button
 navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
@@ -36,15 +36,31 @@ contactBtn.addEventListener("click", () => {
   scrollToView("#contact");
 });
 
-function scrollToView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
-
-//
+// Make home to transparent as the window scrolls down
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
+});
+
+// Handle click on the arrow button
+arrowUp.addEventListener("click", () => {
+  scrollToView("#home");
+});
+
+function scrollToView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
