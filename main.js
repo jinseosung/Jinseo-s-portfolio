a29122bd88cde41cb705074d73d9bd5d08ec5a40
Jinseo Sung <jinseosung15@gmail.com>
2022-09-01 11:42:16 +0200

    JS : Navbar menu click modified

--- main.js
+++ main.js
@@ -20,19 +20,23 @@ toggleBtn.addEventListener("click", () => {
   navbarMenu.classList.toggle("navbar--selected");
 });
 
-//Handle scrolling when tapping on the navbar menu
-const contactBtn = document.querySelector(".home__contact");
-
-function scrollToView(event) {
+//Handle scrolling when tapping on the navbar menu and "contact me" button
+navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link === null) {
     return;
   }
 
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
-}
+  scrollToView(link);
+});
 
-navbarMenu.addEventListener("click", scrollToView);
-contactBtn.addEventListener("click", scrollToView);
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", ()=>{
+    scrollToView('#contact');
+});
+
+function scrollToView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+  }
